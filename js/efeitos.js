6231834585a0805fbcb21a6345f05d3918f95a21
Talisson Mauricio <talissonmnunes@hotmail.com>
2018-03-01 11:54:14 -0300

    Responsividade - Menu Mobile Completo

--- js/efeitos.js
+++ js/efeitos.js
@@ -32,11 +32,13 @@ $(document).ready(function(){
   	$('#btn-news-prev').on("click", function(){
 
   		owl.prev();
+
   	});
 
   	$('#btn-news-next').on("click", function(){
 
   		owl.next();
+
   	});
 
   	$("#page-up").on("click", function(event){
@@ -46,6 +48,27 @@ $(document).ready(function(){
   		}, 1000);
 
   		event.preventDefault();
+
   	});
+
+    $("#btn-bars").on("click", function(){
+
+      $("header").toggleClass("open-menu");
+
+    });
+
+    $("#menu-mobile-mask, .btn-close").on("click", function(){
+
+      $("header").removeClass("open-menu");
+
+
+    });
+
+    $("#btn-search").on("click", function(){
+
+      $("header").toggleClass("open-search");
+      $("#input-search-mobile").focus();
+
+    });
 
 });
